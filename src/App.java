9815f3f9aa86b973e580a752109775e4b72325c6
NoahRosas <nrosas@itpfp.com>
2023-12-01 11:12:58 +0100

    declaro variables

--- src/App.java
+++ src/App.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        /*Desarrollar un juego de preguntas y respuestas en Java que permita a los usuarios responder preguntas y
+        obtener puntajes según sus respuestas.
+        Crear 10 preguntas. (2 compañeros) 15 preguntas (3 compañeros)
+        Almacenar las preguntas en un formato estructurado (puede ser en un array),  y que se muestren en pantalla de forma random.
+        Presentar preguntas al usuario y permitirle escribir una respuesta.
+        Llevar un seguimiento del puntaje del usuario.
+        Utilizar condicionales para verificar si la respuesta del usuario es correcta. */
+        Scanner sc = new Scanner(System.in);
+        String preguntas[]=new String[9];
+        String respuestas[]=new String[9];
+        int contador=0;
+        preguntas[0]="";
+        preguntas[1]="";
+        preguntas[2]="";
+        preguntas[3]="";
+        preguntas[4]="";
+        preguntas[5]="";
+        preguntas[6]="";
+        preguntas[7]="";
+        preguntas[8]="";
+        preguntas[9]="";
+
 
 
 
